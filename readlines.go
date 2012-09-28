fa71a1cb0ab65f2e173783b2c4a0d72da0fba60c
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-09-28 10:34:17 +0200

    ReadLines without buffered channels. Buffering interferes with Break().

--- readlines.go
+++ readlines.go
@@ -83,7 +83,7 @@ func (r *LinesReader) ReadLines() <-chan string {
 	if !r.isOpen {
 		panic("LinesReader is closed")
 	}
-	ch := make(chan string, 100)
+	ch := make(chan string)
 	go func() {
 	ReadLinesLoop:
 		for {
@@ -125,7 +125,7 @@ func (r *LinesReader) ReadLinesBytes() <-chan []byte {
 	if !r.isOpen {
 		panic("LinesReader is closed")
 	}
-	ch := make(chan []byte, 100)
+	ch := make(chan []byte)
 	go func() {
 	ReadLinesLoop:
 		for {
