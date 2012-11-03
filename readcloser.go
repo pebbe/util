ba740b084ca7b11827dec753376e4c8804fd4e26
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-11-03 13:25:20 +0100

    readcloser.go: cleaner, more like true Go

--- readcloser.go
+++ readcloser.go
@@ -8,60 +8,68 @@ import (
 	"strings"
 )
 
-// Implements io.ReadCloser
 type ReadCloser struct {
-	r      io.Reader
-	fp     *os.File
-	gz     *gzip.Reader
-	isOpen bool
-	isGzip bool
-	isBz2  bool
+	r       io.Reader
+	fp      *os.File
+	gz      *gzip.Reader
+	isOpen  bool
+	isGzip  bool
+	isBzip2 bool
 }
 
 // Opens for reading a plain file, gzip'ed file (extension .gz), or bzip2'ed file (extension .bz2)
-func Open(filename string) (rc *ReadCloser, err error) {
-	rc = &ReadCloser{}
-
-	rc.fp, err = os.Open(filename)
+func Open(filename string) (rc io.ReadCloser, err error) {
+	fp, err := os.Open(filename)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	if strings.HasSuffix(filename, ".gz") {
-		rc.gz, err = gzip.NewReader(rc.fp)
-		if err != nil {
-			rc.fp.Close()
-			return
+		r := ReadCloser{
+			fp:     fp,
+			isGzip: true,
+			isOpen: true,
 		}
-		rc.r = rc.gz
-		rc.isGzip = true
-	} else if strings.HasSuffix(filename, ".bz2") {
-		rc.r = bzip2.NewReader(rc.fp)
-		rc.isBz2 = true
-	} else {
-		rc.r = rc.fp
+		r.gz, err = gzip.NewReader(fp)
+		if err != nil {
+			fp.Close()
+			return nil, err
+		}
+		r.r = r.gz
+		return r, nil
 	}
 
-	rc.isOpen = true
-	return
+	if strings.HasSuffix(filename, ".bz2") {
+		r := ReadCloser{
+			fp:      fp,
+			r:       bzip2.NewReader(fp),
+			isBzip2: true,
+			isOpen:  true,
+		}
+		return r, nil
+	}
+
+	return fp, nil
 }
 
-func (rc *ReadCloser) Read(p []byte) (n int, err error) {
+func (rc ReadCloser) Read(p []byte) (n int, err error) {
 	if !rc.isOpen {
 		panic("ReadCloser is closed")
 	}
 	return rc.r.Read(p)
 }
 
-func (rc *ReadCloser) Close() (err error) {
+func (rc ReadCloser) Close() (err error) {
 	if rc.isOpen {
 		rc.isOpen = false
+
 		if rc.isGzip {
 			err = rc.gz.Close()
 		}
-		err2 := rc.fp.Close()
+
+		e := rc.fp.Close()
 		if err == nil {
-			err = err2
+			err = e
 		}
 	}
 	return
